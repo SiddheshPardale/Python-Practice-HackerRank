3741a3e5a32884a966466cf7b06a937646ce6bc0
Siddhesh Pardale <siddheshpardale9409@gmail.com>
2026-08-03 13:15:39 +0530

    Create Loops.py for HackerRank problem
    Add a Python script to demonstrate loops and squares.

---
+++ Python/Loops.py
@@ -0,0 +1,11 @@
+# Problem: Loops
+# Platform: HackerRank
+# Topic: Introduction
+# Difficulty: Easy
+# Link: https://www.hackerrank.com/challenges/python-loops
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i**2)
+
