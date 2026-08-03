c57a278bcf2767077f856af0c211d182ce1e5adb
Siddhesh Pardale <siddheshpardale9409@gmail.com>
2026-08-03 13:13:03 +0530

    Create Division.py for HackerRank challenge
    Add Python division problem solution from HackerRank.

---
+++ Python/Python:Division.py
@@ -0,0 +1,11 @@
+# Problem: Python: Division
+# Platform: HackerRank
+# Topic: Introduction
+# Difficulty: Easy
+# Link: https://www.hackerrank.com/challenges/python-division
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
