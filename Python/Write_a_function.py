c21158a8f641d2a1dc30f55c5949fa1cda58667c
Siddhesh Pardale <siddheshpardale9409@gmail.com>
2026-08-03 13:19:18 +0530

    Add leap year determination function
    Implement a function to determine if a year is a leap year based on the given conditions.

---
+++ Python/Write_a_function.py
@@ -0,0 +1,14 @@
+# Problem: Write a function
+# Platform: HackerRank
+# Topic: Introduction
+# Difficulty: Easy
+# Link: https://www.hackerrank.com/challenges/write-a-function
+
+def is_leap(year):
+    leap = False
+    if (year%4 == 0 and year%100!=0) or (year%400 == 0):
+        leap = True
+    return leap
+
+year = int(input())
+print(is_leap(year))
