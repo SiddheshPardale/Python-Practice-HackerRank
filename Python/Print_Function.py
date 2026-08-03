18e1cab2df707b675c23ef8c9c2c6c5f5338cdde
Siddhesh Pardale <siddheshpardale9409@gmail.com>
2026-08-03 13:21:27 +0530

    Add print function for HackerRank problem
    Implement print function for HackerRank challenge.

---
+++ Python/Print_Function.py
@@ -0,0 +1,10 @@
+# Problem: Print Function
+# Platform: HackerRank
+# Topic: Introduction
+# Difficulty: Easy
+# Link: https://www.hackerrank.com/challenges/python-print
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,n+1):
+        print(i,end="")
